3ed842b28313d12177d0fd7f728a3f49f3c334e1
loulouagr <agrofoglio@telecom-paristech.fr>
2019-10-11 15:41:43 +0200

    ajout d'une classe pour la checklist dynamique

---
+++ src/Admin/ChecklistController.cpp
@@ -0,0 +1,6 @@
+#include "ChecklistController.h"
+
+ChecklistController::ChecklistController()
+{
+
+}
---
+++ src/Admin/ChecklistController.h
@@ -0,0 +1,11 @@
+#ifndef CHECKLISTCONTROLLER_H
+#define CHECKLISTCONTROLLER_H
+
+
+class ChecklistController
+{
+public:
+    ChecklistController();
+};
+
+#endif // CHECKLISTCONTROLLER_H
